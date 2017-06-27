d535c1baef7602e23e5d0c89b526ccb060232b22
uptonking <jinyaoo86@gmail.com>
2017-06-27 14:38:26 +0800

    [add] list stack

--- java8/src/main/java/yao/basic/linear/stack/ArrayStack.java
+++ java8/src/main/java/yao/basic/linear/stack/ArrayStack.java
@@ -1,79 +1,80 @@
 package yao.basic.linear.stack;
 
-import yao.basic.linear.array.Array;
 
 /**
- * Java栈
+ * 数组实现的栈
  */
-public class ArrayStack {
+public class ArrayStack<T> {
 
+    //栈顶指针
     private int top;
 
     private int capacity;
 
     private int[] array;
 
-    public ArrayStack(){
+
+    public ArrayStack(int capacity) {
         this.top = -1;
-        this.capacity=1;
+        this.capacity = capacity;
         this.array = new int[this.capacity];
     }
 
-    public ArrayStack(int capacity){
-        this.top = -1;
-        this.capacity=capacity;
-        this.array = new int[this.capacity];
+    public ArrayStack() {
+        this(1);
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
 
 
         return true;
     }
 
-    public boolean isFull(){
+    public boolean isFull() {
 
         return false;
     }
 
-    public int size(){
+    public int size() {
 
         return -1;
     }
 
-    public boolean push(int x){
+    public void traverse() {
+
+    }
+
+    public int search() {
+
+        return -1;
+    }
+
+    public boolean push(int x) {
 
 
         return true;
     }
 
-    public int pop(){
+    public int pop() {
 
         return -1;
     }
 
-    public int peek(){
+    public int peek() {
 
         return -1;
     }
 
-    public boolean clear(){
-
-        return true;
-    }
-
-    public boolean search(Object o){
+    public boolean clear() {
 
         return true;
     }
 
 
+    public void display() {
 
-    public void display(){
-
-        System.out.println("");
+        System.out.println();
     }
-
 
 
 }

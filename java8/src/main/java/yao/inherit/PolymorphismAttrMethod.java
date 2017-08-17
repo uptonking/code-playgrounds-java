8edca8a1a19fe1b247333de56c15af4bbdacc3d2
uptonking <jinyaoo86@gmail.com>
2017-08-17 17:03:03 +0800

    [misc] 20170817

--- java8/src/main/java/yao/inherit/PolymorphismAttrMethod.java
+++ java8/src/main/java/yao/inherit/PolymorphismAttrMethod.java
@@ -1,37 +1,30 @@
 package yao.inherit;
 
-
 import static java.lang.System.out;
 
-/**
- * Created by yaoooo on 8/9/17.
- */
+class BaseCLass {
 
+    public int book = 1111;
 
-class BaseCLass{
-
-    public int book=1111;
-
-    public void test(){
-        out.println("父类test()打印："+book);
+    public void test() {
+        out.println("父类test()打印：" + book);
     }
 
-    public void base(){
+    public void base() {
         out.println("父类base()执行");
     }
 
 }
 
+class SubCLass extends BaseCLass {
 
-class SubCLass extends BaseCLass{
+    public String book = "子类book变量";
 
-    public String book="子类book变量";
-
-    public void test(){
-        out.println("子类test()打印："+book);
+    public void test() {
+        out.println("子类test()打印：" + book);
     }
 
-    public void sub(){
+    public void sub() {
         out.println("子类sub()执行");
     }
 
@@ -39,21 +32,16 @@ class SubCLass extends BaseCLass{
 
 public class PolymorphismAttrMethod {
 
-    public static void main(String ...args){
+    public static void main(String... args) {
 
-
-        BaseCLass bc  = new SubCLass();
-
+        BaseCLass bc = new SubCLass();
         out.println(bc.book);
 
         bc.test();
-
         bc.base();
 
         //bc编译时无sub方法，下行代码报错
-//        bc.sub();
-
-
+        //bc.sub();
     }
 
 }

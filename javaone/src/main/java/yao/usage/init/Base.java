c2264e16b295c3bc16c4781a0dc9a9853748aa7e
uptonking <jinyaoo86@gmail.com>
2018-02-26 17:37:05 +0800

    add: subclass init sample

--- javaone/src/main/java/yao/usage/init/Base.java
+++ javaone/src/main/java/yao/usage/init/Base.java
@@ -2,7 +2,10 @@ package yao.usage.init;
 
 /**
  * 实例变量的声明赋值会被构造函数赋值覆盖
- * 实例方法可以多态，但实例变量没有多态
+ * <p>
+ * 子类分配内存及初始化之前初始化分类
+ * <p>
+ * 实例方法可以多态，但实例变量没有多态，java里面没有字段的重写
  */
 public class Base {
 
@@ -19,9 +22,19 @@ public class Base {
     }
 
 
-    //程序的入口
+    /**
+     * 程序的入口
+     * <p>
+     * 输出：
+     * ====Sub callName: null
+     * ====Sub callName: sub2
+     */
     public static void main(String[] args) {
+
+        //这里会调用子类的callName()，因为有重写，但是子类变量未分配内存
         Base b = new Sub();
+
+        //调用子类
         b.callName();
 
     }
